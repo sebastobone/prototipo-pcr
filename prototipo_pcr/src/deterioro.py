3fa7c1a442dc80e915adb3b69d2ec8f7317444c1
Prisspr888 <83788268+Prisspr888@users.noreply.github.com>
2025-07-23 15:45:15 -0500

    se ajusta el modulo deterioro segun ultimas definiciones
    la metodologia se homologa entre estandares y los movimientos capturan el cambio en saldo y probabilidad de incumplimiento

--- prototipo_pcr/src/deterioro.py
+++ prototipo_pcr/src/deterioro.py
@@ -1,110 +1,80 @@
 """
-Módulo de cálculo del deterioro del activo por reaseguro bajo IFRS17
+Módulo de cálculo del deterioro del activo por reaseguro
 """
 
 import src.cruces as cruces
+import src.parametros as p
 import polars as pl
 import duckdb
 import datetime as dt
-from typing import List
 
 
-def calcular_deterioro(
-    output_devengo: pl.DataFrame,
+
+def calc_deterioro(
+    output_devengo_fluc: pl.DataFrame,
     riesgo_credito: pl.DataFrame,
-    variables_uoa: List[str],
-    historico_deterioro:pl.DataFrame,
-    tasas_cambio:pl.DataFrame,
     fe_valoracion:dt.date,
 ) -> pl.DataFrame:
-    """
-    Calcula el deterioro de activos por reaseguro según criterios de IFRS17
-    a partir del output de devengo y la probabilidad de incumplimiento
-    Regresa movimientos de deterioro, la constitucion es negativa y la liberacion es positiva
-    """
-
-    agrupadoras = variables_uoa + ["fecha_valoracion"]
-
-    historico_deterioro = historico_deterioro.cast({
-        col: output_devengo.schema[col]
-        for col in agrupadoras if col in output_devengo.columns
-    }).cast({'valor_deterioro': pl.Float64, 'saldo_pcr': pl.Float64, 'probabilidad_incumplimiento': pl.Float64})
-
-    saldo_uoa = output_devengo.filter(
-        # Solo aplica para la fecha de valoracion y para reaseguro
-        (pl.col('fecha_valoracion') == fe_valoracion) & (~pl.col('tipo_negocio').is_in(['directo', 'camara_soat']))
-    ).group_by(agrupadoras).agg(
-        # Totaliza por unidad de cuenta, reasegurador y fecha
-        pl.col("saldo").sum().alias("saldo_pcr")
+    
+    # Solo aplica para la fecha de valoracion y para reaseguro
+    base_det = output_devengo_fluc.filter(
+        (pl.col('fecha_valoracion') == fe_valoracion) & 
+        (pl.col('tipo_negocio').is_in(['cedido', 'retrocedido']))
     )
 
     # Cruza probabilidad de default PD vigente por reasegurador
-    duckdb.register("saldo_uoa", saldo_uoa)
+    duckdb.register("base_det", base_det)
     duckdb.register("riesgo_credito", riesgo_credito)
     deterioro_pcr = duckdb.sql(
         """
         SELECT 
             pcr.*
-            ,pd.probabilidad_incumplimiento
-        FROM saldo_uoa as pcr
+            ,pd.probabilidad_incumplimiento prob_incumplimiento_actual
+            ,pd_ant.probabilidad_incumplimiento prob_incumplimiento_anterior
+        FROM base_det as pcr
             LEFT JOIN riesgo_credito as pd
                 ON pcr.nit_reasegurador = pd.nit_reasegurador
                 AND pcr.fecha_valoracion BETWEEN 
                     pd.fecha_inicio_vigencia AND 
                         COALESCE(pd.fecha_fin_vigencia, '3000-12-31')
+            LEFT JOIN riesgo_credito as pd_ant
+                ON pcr.fecha_valoracion_anterior BETWEEN 
+                    pd_ant.fecha_inicio_vigencia AND 
+                        COALESCE(pd_ant.fecha_fin_vigencia, '3000-12-31')
         """
-    ).pl()
-    
-    if historico_deterioro.is_empty():
-        # Garantiza que 'liberacion' exista aunque el join no se haga
-        deterioro_pcr = deterioro_pcr.with_columns(pl.lit(0.0).alias('liberacion'))
-    else:
-        deterioro_pcr = deterioro_pcr.join(
-            historico_deterioro
-                .filter(pl.col('fecha_valoracion') < fe_valoracion)
-                .sort("fecha_valoracion")
-                .group_by(variables_uoa)
-                .tail(1)
-                .rename({"valor_deterioro": "liberacion"})
-                .select(variables_uoa + ["liberacion"]),
-            how='left',
-            on=variables_uoa
-        ).with_columns(
-            pl.col("liberacion").fill_null(0.0)
-        )
-
-    deterioro_pcr = deterioro_pcr.with_columns([
-        pl.when(pl.col("saldo_pcr") > 0)
-        .then(pl.col("saldo_pcr") * pl.col("probabilidad_incumplimiento"))
-        .otherwise(0.0).alias("valor_deterioro")
-        ]).filter(pl.col('valor_deterioro') > 0)
-    
-    # Actualiza el historico con el ultimo deterioro
-    historico_actualizado = pl.concat(
-        [historico_deterioro, deterioro_pcr.select(historico_deterioro.columns)], how='diagonal')
-
-    # pivot para que quede de una vez por tipo de movimiento
-    output_deterioro = (
-        deterioro_pcr
-        .rename({'valor_deterioro': 'constitucion'})
-        .select(agrupadoras + ['saldo_pcr', 'probabilidad_incumplimiento', 'liberacion', 'constitucion'])
-        .unpivot(
-            index=agrupadoras + ['saldo_pcr', 'probabilidad_incumplimiento'],
-            variable_name='tipo_movimiento',
-            value_name='valor_md'
-        )
-        # calcula valor en pesos con tasa de cambio de valoracion
-        .pipe(cruces.cruzar_tasas_cambio, tasas_cambio, False, False, False)
-        .with_columns(
-            ((   # constitucion negativa y liberacion positiva
-                pl.when(pl.col('tipo_movimiento')=='constitucion')
-                .then(pl.lit(-1))
-                .otherwise(pl.lit(1))
-            ) * pl.col('valor_md'))
-            .alias('valor_md')
-        ).with_columns(
-            (pl.col('valor_md') * pl.col('tasa_cambio_fecha_valoracion')).alias('valor_ml')
-        )
+    ).pl(
+    ).with_columns(
+        (pl.col('prob_incumplimiento_actual') - pl.col('prob_incumplimiento_anterior'))
+        .alias('cambio_prob_incumplimiento')
     )
 
-    return output_deterioro, historico_actualizado
+    # calculo de movimientos de deterioro
+    # se constituye deterioro si la probabilidad de default aumenta
+    constitucion_det = pl.col('saldo') * pl.max_horizontal(pl.col('cambio_prob_incumplimiento'), pl.lit(0.0))
+    # la liberación tiene dos partes
+    # se libera porque el saldo cambia
+    lib_cambio_saldo = pl.min_horizontal(pl.col('saldo') - pl.col('saldo_anterior'), pl.lit(0.0)) * pl.col('prob_incumplimiento_anterior')
+    # se libera si baja la probabilidad
+    lib_cambio_pd = pl.min_horizontal(pl.col('cambio_prob_incumplimiento'), pl.lit(0.0)) * pl.col('saldo')
+    deterioro_pcr = deterioro_pcr.with_columns([
+        constitucion_det.alias("constitucion_deterioro"),
+        (lib_cambio_saldo + lib_cambio_pd).alias('liberacion_deterioro')
+    ])
+
+    # la parte a la cual no le aplica el deterioro tendra NA en las columnas creadas
+    base_resto = output_devengo_fluc.filter(
+        ~((pl.col('fecha_valoracion') == fe_valoracion) & 
+        (pl.col('tipo_negocio').is_in(['cedido', 'retrocedido'])))
+    ).with_columns([
+        # crea las columnas de deterioro con nulos para poder combinar
+        pl.lit(None).cast(pl.Float64).alias(det_col)
+        for det_col in [
+            'prob_incumplimiento_actual', 
+            'prob_incumplimiento_anterior',
+            'cambio_prob_incumplimiento', 
+            'constitucion_deterioro', 
+            'liberacion_deterioro', 
+            ]
+    ])
+
+    return base_resto.vstack(deterioro_pcr)
