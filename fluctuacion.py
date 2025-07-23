99cb2c841fa74ac0523b5efb13cbfdaa5cde6bdb
Prisspr888 <83788268+Prisspr888@users.noreply.github.com>
2025-07-23 15:42:03 -0500

    se ajusta la fluctuacion a la ultima definicion
    se aplican reglas de negocio de la fluctuacion homologada entre estandares contables

---
+++ fluctuacion.py
@@ -0,0 +1,48 @@
+"""
+Funciones para el cálculo de la fluctuación por tasa de cambio de los componentes de la reserva
+se condiciona porque la metodología cambia para ifrs17 vs ifrs4
+"""
+
+import src.aux_tools as aux_tools
+import src.cruces as cruces
+import polars as pl
+
+
+def calc_fluctuacion(data_devengo:pl.DataFrame, tasas_cambio:pl.DataFrame) -> pl.DataFrame:
+
+    # Filtra para alicar solo a moneda extranjera
+    data_devengo_mext = data_devengo.filter(pl.col('moneda') != "COP")
+    data_devengo_mloc = data_devengo.filter(~(pl.col('moneda') != "COP"))
+
+    # Cambios en tasa de cambio para usar segun el tipo de contabilidad
+    # incluir columnas de estos deltas en el output
+    delta_tc_bautizo = pl.col('tasa_cambio_fecha_valoracion') - pl.col('tasa_cambio_fecha_constitucion')
+    delta_tc_mensual = pl.col('tasa_cambio_fecha_valoracion') - pl.col('tasa_cambio_fecha_valoracion_anterior')
+    
+    # usa el cambio en tasa respecto a la fecha de bautizo cuando es el primer mes de devengo
+    es_mes_inicio = aux_tools.yyyymm(pl.col('fecha_constitucion')) == aux_tools.yyyymm(pl.col('fecha_valoracion'))
+    delta_tc = pl.when(es_mes_inicio).then(delta_tc_bautizo).otherwise(delta_tc_mensual)
+
+    data_devengo_mext = data_devengo_mext.with_columns(
+        # define fecha cierre anterior para el delta
+        pl.col('fecha_valoracion').dt.offset_by('-1mo').alias('fecha_valoracion_anterior')
+    ).pipe(cruces.cruzar_tasas_cambio, tasas_cambio, liquidacion=False).with_columns(
+        (pl.col('saldo') * delta_tc)
+        .alias('fluctuacion_constitucion')
+    ).with_columns(
+        # El signo de la liberacion se invierte para reflejar el efecto economico
+        (-1 * pl.col('valor_liberacion') * delta_tc)
+        .alias('fluctuacion_liberacion')
+    )
+    cols_fluc =  ["fluctuacion_constitucion", "fluctuacion_liberacion"]
+    cols_tasas = [c for c in data_devengo_mext.columns if "tasa_cambio_" in c]
+    # concatena con los no fluctuados y llena nulos
+    data_devengo_fluc = pl.concat(
+            [data_devengo_mloc, data_devengo_mext], how='diagonal').with_columns([
+                pl.col(col).fill_null(0.0) for col in cols_fluc
+            ]).with_columns([
+                pl.col(col).fill_null(1.0) for col in cols_tasas
+            ])
+    
+    return data_devengo_fluc
+    
